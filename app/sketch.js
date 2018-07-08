03210265abbe32d6e8d8b9e0227f9953d80376b1
Tharuka <tharukavishwajiths@gmail.com>
2018-07-08 19:59:27 +0530

    connected to firebase

--- app/sketch.js
+++ app/sketch.js
@@ -1,21 +1,69 @@
+let r,g,b;
+let database;
+
+var config = {
+  apiKey: "AIzaSyBz0E9GGdLmcyDh9ODadXYTU0UlLer5qks",
+  authDomain: "color-classifire.firebaseapp.com",
+  databaseURL: "https://color-classifire.firebaseio.com",
+  projectId: "color-classifire",
+  storageBucket: "",
+  messagingSenderId: "216907298724"
+};
+firebase.initializeApp(config);
+database = firebase.database();
+
+function pickColor(){
+  r = floor(random(255));
+  g = floor(random(255));
+  b = floor(random(255));
+  background(r,g,b);
+}
+
 function setup() {
   createCanvas(400,400);
-  let r = floor(random(255));
-  let g = floor(random(255));
-  let b = floor(random(255));
-  background(r,g,b);
+  pickColor();
+  
+  let buttons = [];
+  buttons.push(createButton('red-ish'));
+  buttons.push(createButton('blue-ish'));
+  buttons.push(createButton('green-ish'));
+  buttons.push(createButton('orange-ish'));
+  buttons.push(createButton('yello-ish'));
+  buttons.push(createButton('pink-ish'));
+  buttons.push(createButton('purple-ish'));
+  buttons.push(createButton('brown-ish'));
+  buttons.push(createButton('gray-ish'));
 
-  let radioButton = createRadio();
-  radioButton.option('red-is');
-  radioButton.option('blue-is');
-  radioButton.option('green-is'); 
-
-  let submit = createButton('Submit');
-  submit.mousePressed(sendData);
+  for(let i = 0; i < buttons.length; i++ ){
+    buttons[i].mousePressed(sendData)
+  }
+  
 
   function sendData(){
-    
+    let colorDatabase = database.ref('colors');
+
+    var data = {
+      r: r,
+      g: g,
+      b: b,
+      lable: this.html()
+    }
+    let color = colorDatabase.push(data,finished)
+    console.log('Saving data');
+    console.log(data);
+
+    function finished(err){
+      if(err){
+        console.log('Somting is wrong!');
+        console.error(err);
+      }else{
+        console.log('Data saved success!');
+        pickColor();
+      }
+    }
   }
+
+  
  
 }
 
